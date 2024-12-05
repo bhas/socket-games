20c02b0c74602553c00147162b66800876ef5336
BJORN-VINTHER <g.bvinther@gmail.com>
2024-12-05 20:23:56 +0100

    update layout

--- ui/src/pages/Layout.tsx
+++ ui/src/pages/Layout.tsx
@@ -1,12 +1,14 @@
 import { Link, Outlet } from "react-router";
 
-
 export default function Layout() {
-    return (
-        <>
-        <header>I am a header</header>
-        <Outlet/>
-        <footer>I am a footer</footer>
-        </>
-    )
+  return (
+    <>
+      <header className="bg-blue-300 p-4 flex flex-row items-center justify-center">
+        My First Game
+      </header>
+      <main className="mx-auto max-w-4xl border-2 p-10">
+        <Outlet />
+      </main>
+    </>
+  );
 }
