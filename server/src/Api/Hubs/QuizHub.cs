da8cff158e4ba9a27353a668c185f74e73fecc59
BJORN-VINTHER <g.bvinther@gmail.com>
2024-12-05 21:07:17 +0100

    configured basic SignalR

---
+++ server/src/Api/Hubs/QuizHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNetCore.SignalR;
+namespace SignalRChat
+{
+    public class QuizHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task Celebrate(string user)
+        {
+            await Clients.All.SendAsync("ReceiveCelebration", user);
+        }
+    }
+}
