18f1cc33bd95711e5bfc9c17f7341991db2596ae
Bjørn Vinther <bjorn.vinther@lego.com>
2025-06-06 15:53:36 +0200

    add cors

--- server/src/Api/Program.cs
+++ server/src/Api/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddSignalR();
 
 builder.Services.AddSingleton(new ServiceState());
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowLocalhost", builder =>
     {
-        builder.WithOrigins("http://localhost:5173")
+        builder.WithOrigins("http://localhost:5173", "https://fabulab-ui.bvinther.com")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
@@ -27,10 +27,10 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+    app.UseSwagger();
+    app.UseSwaggerUI();
 if (app.Environment.IsDevelopment())
 {
-    //app.UseSwagger();
-    //app.UseSwaggerUI();
     app.UseExceptionHandler("/Error");
     app.UseHsts();
 }
@@ -40,5 +40,7 @@ app.UseHttpsRedirection();
 //app.UseStaticFiles();
 //app.UseRouting();
 app.MapHub<QuizHub>("quiz");
+
+app.MapGet("/dummy", () => "This is a dummy endpoint");
 
 app.Run();
